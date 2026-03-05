a887718748c69d48c65ae6f7e2d6209e74ce6f49
samidha1-1 <samidhawani72gmail.com>
2026-03-05 20:34:13 +0530

    feat:My flask app file

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello from Flask on Docker and EC2!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
